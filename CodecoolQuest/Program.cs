f47c04066d483bdee12a75ace01b56f81145ee53
KacperMitkowski <43199382+KacperMitkowski@users.noreply.github.com>
2020-06-18 16:14:00 +0200

    Changed directories names

--- CodecoolQuest/Program.cs
+++ CodecoolQuest/Program.cs
@@ -11,6 +11,7 @@ namespace Codecool.Quest
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
+
         static void Main()
         {
             using var game = new Game();
